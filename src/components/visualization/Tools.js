c0966a0c1791f2ab0743c5a2ebf87e4287b0192d
bea92 <gobbo.beatrice92@gmail.com>
2021-07-29 11:49:26 +0200

    add style to gt

--- src/components/visualization/Tools.js
+++ src/components/visualization/Tools.js
@@ -10,7 +10,7 @@ import {
   BsMap as ToursIcon,
 } from "react-icons/bs";
 import { Tabs, Tab } from "react-bootstrap";
-import classNames from "classnames"
+import classNames from "classnames";
 
 import Clusters from "../../images/clusters.svg";
 import Projects from "../../images/projects.svg";
@@ -53,9 +53,9 @@ const Tools = ({
       <div className={classNames(styles.tools)}>
         <div className={ClassNames(styles.panel, { [styles.open]: panel })}>
           <div className={ClassNames(styles.panelHeader)}>
-            <h3 className={ClassNames("text-uppercase", styles.title)}>
+            <h4 className={ClassNames("text-uppercase", styles.title)}>
               Exploration modes
-            </h3>
+            </h4>
             <div></div>
             {panel && (
               <ClosePanelIcon
@@ -78,40 +78,57 @@ const Tools = ({
                 changeVizMode(k);
               }}
               id="explorationMode"
+              className={styles.tour}
             >
-              <Tab eventKey="tours" title={<ToursIcon />}>
+              <Tab
+                eventKey="tours"
+                title={<ToursIcon />}
+                className={styles.customTab}
+              >
                 <h5>Take a guided tour</h5>
+                <p>Explore the space taking a thematic tour.</p>
                 {tours.map((d) => (
-                  <h6
+                  <ul className={styles.listTour}><h6
                     key={d.id}
                     onClick={() => {
                       goOnTour(d);
                     }}
                   >
                     {d.title}
-                  </h6>
+                  </h6></ul>
                 ))}
               </Tab>
-              <Tab eventKey="clusters" title="Clusters">
+              <Tab
+                eventKey="clusters"
+                title="Clusters"
+                className={styles.customTab}
+              >
+                <h5>How to read the visualization</h5>
                 <img
                   src={Clusters}
                   className={styles.legend}
                   alt="legend"
                 ></img>
               </Tab>
-              <Tab eventKey="projects" title="Projects">
+              <Tab
+                eventKey="projects"
+                title="Projects"
+                className={styles.customTab}
+              >
+                <h5>How to read the visualization</h5>
                 <img
                   src={Projects}
                   className={styles.legend}
                   alt="legend"
                 ></img>
               </Tab>
-              <Tab eventKey="networks" title="Networks">
-                <img
-                  src={Tactics}
-                  className={styles.legend}
-                  alt="legend"
-                ></img>
+              <Tab
+                eventKey="networks"
+                title="Networks"
+                className={styles.customTab}
+              >
+                <h5>How to read the visualization</h5>
+                <img src={Tactics} className={styles.legend} alt="legend"></img>
               </Tab>
             </Tabs>
           </div>
@@ -121,6 +138,7 @@ const Tools = ({
             dangerouslySetInnerHTML={{
               __html: activeTour.steps[tourStep].text,
             }}
+            className={styles.toursBox}
           />
         )}
       </div>
@@ -131,16 +149,18 @@ const Tools = ({
               variant="light"
               onClick={() => moveTour("prev")}
               disabled={tourStep === 0}
+              className={styles.toursButton}
             >
               {"<"}
             </Button>
-            <Button variant="light" onClick={() => exitTour()}>
+            <Button variant="light" onClick={() => exitTour()} className={styles.toursButton}>
               End Tour
             </Button>
             <Button
               variant="light"
               onClick={() => moveTour("next")}
               disabled={tourStep === activeTour.steps.length - 1}
+              className={styles.toursButton}
             >
               {">"}
             </Button>
